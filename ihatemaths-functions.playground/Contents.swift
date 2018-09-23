73e1eab2ca0ffd45b7d7d770eb8ca471cf1529ea
Krystian Gontarek <krystiangontarek@MacBook-Pro-Krystian.local>
2018-09-23 22:34:46 +0200

    Cases for different absolute actions added

--- ihatemaths-functions.playground/Contents.swift
+++ ihatemaths-functions.playground/Contents.swift
@@ -7,23 +7,46 @@
 func movePoint(point: (x: Int, y: Int), operations: (lr: Int, ud: Int), absolute: String) {
     // i really hate maths man
     // sample point that is part of the function
-    let currentPoint = point
-    print(currentPoint, " \n \n | \n | \n V \n")
+    point
+    print(point, " \n \n | \n | \n V \n")
     
     // we pass sample point coordinates to our new point, so we can move it using vector
-    var newPoint: (x: Int, y: Int) = currentPoint
+    var newPoint: (x: Int, y: Int) = point
     
-    // left/right movement
-    newPoint.x += operations.lr * -1
-    // up/down movement
-    newPoint.y += operations.ud
+
     
     // y-axis symmetry
     // reflect only for positive x'es (and vibes)
-    if absolute == "yes" && newPoint.0 > 0 {
-        newPoint.0 *= -1
+    if absolute == "on_x_only" && newPoint.x > 0 {
+        // f(x) = |x|
+        
+        // left/right movement
+        newPoint.x += operations.lr * -1
+        // up/down movement
+        newPoint.y += operations.ud
+        newPoint.x *= -1
+    } else if absolute == "on_x_operations" {
+        // f(x) = |x - a|
+        
+        newPoint.x *= -1
+        newPoint.x += operations.lr * -1
+        newPoint.y += operations.ud
+    } else if absolute == "on_func_argument" && newPoint.y < 0 {
+        // y = |f(x)|
+        
+        newPoint.x += operations.lr * -1
+        newPoint.y *= -1
+        newPoint.y += operations.ud
+    } else if absolute == "on_func_whole" {
+        newPoint.x += operations.lr * -1
+        if (newPoint.y < 0) {
+            newPoint.y *= -1
+        }
+        newPoint.y += operations.ud
+        newPoint.x *= -1
     }
+    
     print(newPoint)
 }
 
-movePoint(point: (x: -3, y: 3), operations: (lr: -9, ud: +4), absolute: "yes")
+movePoint(point: (x: 1, y: 2), operations: (lr: -2, ud: 0), absolute: "on_func_whole")
